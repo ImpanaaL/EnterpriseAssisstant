7052e7561c559b5dc680fe721ad0bc8284a41377
ImpanaaL <assignwork2024@gmail.com>
2026-06-13 15:40:43 -0400

    second commit

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
 import streamlit as st
+from dotenv import load_dotenv
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+# Load API key from .env
+load_dotenv()
+
+# Initialize Gemini
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash-lite",
+    google_api_key=os.getenv("GOOGLE_API_KEY")
+)
 
 st.title("Safety & Compliance Knowledge Assistant")
 
-st.write("Project setup successful!")    
+question = st.text_input("Ask a question:")
+
+if st.button("Submit"):
+    if question:
+        response = llm.invoke(question)
+        st.write(response.content)
+    else:
+        st.warning("Please enter a question.")
