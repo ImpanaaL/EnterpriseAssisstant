3f27f6012d8ec0022af1aa5679487ba963e2db9f
ImpanaaL <assignwork2024@gmail.com>
2026-06-20 14:08:40 -0400

    Extraction commit

--- app.py
+++ app.py
@@ -1,18 +1,39 @@
 import streamlit as st
 from dotenv import load_dotenv
 import os
+from pypdf import PdfReader
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-# Load API key from .env
 load_dotenv()
 
-# Initialize Gemini
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash-lite",
     google_api_key=os.getenv("GOOGLE_API_KEY")
 )
 
 st.title("Safety & Compliance Knowledge Assistant")
+
+uploaded_file = st.file_uploader("Upload your safety/compliance PDF", type=["pdf"])
+
+if uploaded_file is not None:
+    pdf_reader = PdfReader(uploaded_file)
+
+    text = ""
+
+    for page in pdf_reader.pages:
+        page_text = page.extract_text()
+        if page_text:
+            text += page_text + "\n"
+
+    st.success("PDF uploaded successfully!")
+    st.write(f"Total pages: {len(pdf_reader.pages)}")
+
+    st.subheader("Extracted PDF Text")
+    st.text_area("PDF Content", text, height=300)
+
+st.divider()
+
+st.subheader("Ask a general question")
 
 question = st.text_input("Ask a question:")
 
