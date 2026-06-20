e0514a5114c55ba74730e3d252b8927f2b58c558
ImpanaaL <assignwork2024@gmail.com>
2026-06-20 14:20:46 -0400

    Added PDF chunking functionality

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 from pypdf import PdfReader
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
@@ -30,6 +31,24 @@ if uploaded_file is not None:
 
     st.subheader("Extracted PDF Text")
     st.text_area("PDF Content", text, height=300)
+
+    text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=300,
+    chunk_overlap=50
+)
+
+    chunks = text_splitter.split_text(text)
+
+    st.subheader("Chunking Result")
+    st.write(f"Total chunks created: {len(chunks)}")
+
+    for i, chunk in enumerate(chunks[:3]):
+        st.write(f"Chunk {i + 1}")
+        st.text_area(
+            f"Preview Chunk {i + 1}",
+            chunk,
+            height=150
+        )
 
 st.divider()
 
